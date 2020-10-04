4e8a90e58e7da6cf6501709651bc3712420b2d4b
Kevin Valias <Kevbodyworks@gmail.com>
2020-10-04 09:17:28 -0500

    Finished Product

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route, Redirect } from 'react-router'
 import cookie from 'cookie'
 import Home from './components/Home'
 import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import Car from './components/Car';
+import Login from './components/Login';
+
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
 
 
 // Write ProtectedRoute function here
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies['loggedIn'] ? true : false
+}
 
-
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    return(
+        <Route
+            {...rest}
+            render={(props) =>
+                checkAuth() ? (
+                     <Component {...props} />
+                ) : (
+                      <Redirect to="/login" />
+                )
+            }
+         />
+     )
+}
 const Router = () => {
     return (
         <Switch>
+            <ProtectedRoute path="/about" component={About} />
+            <ProtectedRoute exact path="/" component={Home} />
             <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
+            <ProtectedRoute path="/car/:id" component={Car} />
+            <Route
+                path='*'
+                component={() =>
+                    'This is not the page your looking for.(404)'
+                }
+             />
         </Switch>
     );
 };
